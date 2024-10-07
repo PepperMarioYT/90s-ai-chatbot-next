deda7090050c7f10db11f0e5fdfc1cddb3084c61
PepperMarioYT <142949229+PepperMarioYT@users.noreply.github.com>
2024-10-07 10:45:27 -0400

    90s

--- components/login-form.tsx
+++ components/login-form.tsx
@@ -29,19 +29,21 @@ export default function LoginForm() {
       action={dispatch}
       className="flex flex-col items-center gap-4 space-y-3"
     >
-      <div className="w-full flex-1 rounded-lg border bg-white px-6 pb-4 pt-8 shadow-md  md:w-96 dark:bg-zinc-950">
-        <h1 className="mb-3 text-2xl font-bold">Please log in to continue.</h1>
+      <div className="w-full flex-1 rounded-lg border border-pink-500 bg-cyan-50 px-6 pb-4 pt-8 shadow-lg md:w-96">
+        <h1 className="mb-3 text-3xl font-bold text-blue-700 drop-shadow-lg">
+          Please log in to continue.
+        </h1>
         <div className="w-full">
           <div>
             <label
-              className="mb-3 mt-5 block text-xs font-medium text-zinc-400"
+              className="mb-3 mt-5 block text-xs font-medium text-purple-600"
               htmlFor="email"
             >
               Email
             </label>
             <div className="relative">
               <input
-                className="peer block w-full rounded-md border bg-zinc-50 px-2 py-[9px] text-sm outline-none placeholder:text-zinc-500 dark:border-zinc-800 dark:bg-zinc-950"
+                className="peer block w-full rounded-md border-2 border-yellow-300 bg-white px-2 py-[9px] text-sm outline-none placeholder:text-gray-500"
                 id="email"
                 type="email"
                 name="email"
@@ -52,14 +54,14 @@ export default function LoginForm() {
           </div>
           <div className="mt-4">
             <label
-              className="mb-3 mt-5 block text-xs font-medium text-zinc-400"
+              className="mb-3 mt-5 block text-xs font-medium text-purple-600"
               htmlFor="password"
             >
               Password
             </label>
             <div className="relative">
               <input
-                className="peer block w-full rounded-md border bg-zinc-50 px-2 py-[9px] text-sm outline-none placeholder:text-zinc-500 dark:border-zinc-800 dark:bg-zinc-950"
+                className="peer block w-full rounded-md border-2 border-yellow-300 bg-white px-2 py-[9px] text-sm outline-none placeholder:text-gray-500"
                 id="password"
                 type="password"
                 name="password"
@@ -75,7 +77,7 @@ export default function LoginForm() {
 
       <Link
         href="/signup"
-        className="flex flex-row gap-1 text-sm text-zinc-400"
+        className="flex flex-row gap-1 text-sm text-blue-500"
       >
         No account yet? <div className="font-semibold underline">Sign up</div>
       </Link>
@@ -88,7 +90,7 @@ function LoginButton() {
 
   return (
     <button
-      className="my-4 flex h-10 w-full flex-row items-center justify-center rounded-md bg-zinc-900 p-2 text-sm font-semibold text-zinc-100 hover:bg-zinc-800 dark:bg-zinc-100 dark:text-zinc-900 dark:hover:bg-zinc-200"
+      className="my-4 flex h-10 w-full flex-row items-center justify-center rounded-md bg-green-600 p-2 text-sm font-semibold text-white hover:bg-green-700"
       aria-disabled={pending}
     >
       {pending ? <IconSpinner /> : 'Log in'}
